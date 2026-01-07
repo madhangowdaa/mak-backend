e8aa6d4f5991a818e54c300a24ffe1148cf39e99
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-07 17:03:52 +0530

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import {
 	addMovieController,
+	updateMovieController,
+	deleteMovieController,
 	getAllMoviesController,
 } from '../controllers/movieController.js';
 import { verifyAdmin } from '../middleware/auth.js';
@@ -9,5 +11,7 @@ const router = express.Router();
 
 router.get('/movies', getAllMoviesController);
 router.post('/addMovie', verifyAdmin, addMovieController);
+router.put('/updateMovie', verifyAdmin, updateMovieController);
+router.delete('/deleteMovie', verifyAdmin, deleteMovieController);
 
 export default router;
