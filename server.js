ba00660994ef6d71be97d4cbb315a9da1dc274b5
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-09 17:21:30 +0530

    Add series router to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import moviesRouter from './routes/movies.js';
+import seriesRouter from './routes/series.js';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', moviesRouter);
+app.use('/api/series', seriesRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
