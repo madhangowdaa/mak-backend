2f85085b3435f6bec506551c935c54fc8e0db953
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-09 22:38:07 +0530

    Add new route for popular TMDB API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import moviesRouter from './routes/movies.js';
 import seriesRouter from './routes/series.js';
 import tmdbRouter from "./routes/tmdb.js";
+import tmdbRoutes from "./routes/tmdbRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api', moviesRouter);
 app.use('/api/series', seriesRouter);
 app.use("/api/tmdb", tmdbRouter);
+app.use("/api/populartmdb", tmdbRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
