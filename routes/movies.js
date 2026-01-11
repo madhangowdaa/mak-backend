6ec5c09918a7e4c2510b027bff73c0b6437e4baf
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-11 15:34:15 +0530

    Document query params for getAllMovies route
    Added query parameters information for getAllMovies endpoint.

--- routes/movies.js
+++ routes/movies.js
@@ -9,7 +9,7 @@ import { verifyAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/movies', getAllMoviesController);
+router.get('/movies', getAllMoviesController); // query params: ?page=1&limit=20&q=batman&sort=latest
 router.post('/addMovie', verifyAdmin, addMovieController);
 router.put('/updateMovie', verifyAdmin, updateMovieController);
 router.delete('/deleteMovie', verifyAdmin, deleteMovieController);
