60a29a7b66a6beff59df984feaf44538e9515df0
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-13 17:17:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import seriesRouter from './routes/series.js';
 import tmdbRouter from "./routes/tmdb.js";
 import tmdbRoutes from "./routes/tmdbRoutes.js";
 import carouselRoutes from "./routes/carouselRoutes.js";
+import top10Routes from "./routes/top10.js";
 
 dotenv.config();
 
@@ -13,12 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/api', moviesRouter);
 app.use('/api/series', seriesRouter);
 app.use("/api/tmdb", tmdbRouter);
 app.use("/api/populartmdb", tmdbRoutes);
 app.use("/api/carousel", carouselRoutes);
 app.use("/uploads", express.static("uploads")); // serve uploaded images
-
+app.use("/api", top10Routes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
