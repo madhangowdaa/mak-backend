cc65c67af2938f189c55df1abaaa0cd952d7c0ba
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-13 10:58:19 +0530

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,17 @@
+//routes/movies.js
 import express from 'express';
 import {
 	addMovieController,
 	updateMovieController,
 	deleteMovieController,
 	getAllMoviesController,
+	getRecentMoviesController,
 } from '../controllers/movieController.js';
+
 import { verifyAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
-
+router.get('/movies/recent', getRecentMoviesController);
 router.get('/movies', getAllMoviesController); // query params: ?page=1&limit=20&q=batman&sort=latest
 router.post('/addMovie', verifyAdmin, addMovieController);
 router.put('/updateMovie', verifyAdmin, updateMovieController);
