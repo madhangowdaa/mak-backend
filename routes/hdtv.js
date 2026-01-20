b46725af98988cbec4b03c4e9facad33f4ebc3eb
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-20 16:21:22 +0530

    Create hdtv.js

---
+++ routes/hdtv.js
@@ -0,0 +1,21 @@
+// routes\hdtv.js
+import express from "express";
+import {
+    addHDTVController,
+    updateHDTVController,
+    deleteHDTVController,
+    getAllHDTVController,
+    handleHDTVClickController
+} from "../controllers/hdtvController.js";
+
+import { verifyAdmin } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/hdtv", getAllHDTVController);
+router.post("/hdtv/add", verifyAdmin, addHDTVController);
+router.put("/hdtv/update", verifyAdmin, updateHDTVController);
+router.delete("/hdtv/delete", verifyAdmin, deleteHDTVController);
+router.post("/hdtv/click/:tmdbID", handleHDTVClickController);
+
+export default router;
