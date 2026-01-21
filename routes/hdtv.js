ffec86509857288e73831cf6632f1b8e4c4e7085
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-21 17:55:51 +0530

    Update hdtv.js

--- routes/hdtv.js
+++ routes/hdtv.js
@@ -1,4 +1,3 @@
-// routes\hdtv.js
 import express from "express";
 import {
     addHDTVController,
@@ -7,15 +6,36 @@ import {
     getAllHDTVController,
     handleHDTVClickController
 } from "../controllers/hdtvController.js";
-
 import { verifyAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/hdtv", getAllHDTVController);
-router.post("/hdtv/add", verifyAdmin, addHDTVController);
-router.put("/hdtv/update", verifyAdmin, updateHDTVController);
-router.delete("/hdtv/delete", verifyAdmin, deleteHDTVController);
-router.post("/hdtv/click/:tmdbID", handleHDTVClickController);
+router.post("/hdtv", verifyAdmin, addHDTVController);
+router.put("/hdtv/:id", verifyAdmin, updateHDTVController);
+router.delete("/hdtv/:id", verifyAdmin, deleteHDTVController);
+router.post("/hdtv/click/:id", handleHDTVClickController);
 
 export default router;
+
+// // routes\hdtv.js
+// import express from "express";
+// import {
+//     addHDTVController,
+//     updateHDTVController,
+//     deleteHDTVController,
+//     getAllHDTVController,
+//     handleHDTVClickController
+// } from "../controllers/hdtvController.js";
+
+// import { verifyAdmin } from "../middleware/auth.js";
+
+// const router = express.Router();
+
+// router.get("/hdtv", getAllHDTVController);
+// router.post("/hdtv/add", verifyAdmin, addHDTVController);
+// router.put("/hdtv/update", verifyAdmin, updateHDTVController);
+// router.delete("/hdtv/delete", verifyAdmin, deleteHDTVController);
+// router.post("/hdtv/click/:tmdbID", handleHDTVClickController);
+
+// export default router;
