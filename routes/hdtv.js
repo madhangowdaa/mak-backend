6bab36b4d3d24243a01cd72481fbb25fe6d2b06b
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-01-22 17:29:25 +0530

    Update hdtv.js

--- routes/hdtv.js
+++ routes/hdtv.js
@@ -1,10 +1,12 @@
+// routes\hdtv.js
 import express from "express";
 import {
     addHDTVController,
     updateHDTVController,
     deleteHDTVController,
     getAllHDTVController,
-    handleHDTVClickController
+    handleHDTVClickController,
+    getHDTVByTitleController
 } from "../controllers/hdtvController.js";
 import { verifyAdmin } from "../middleware/auth.js";
 
@@ -15,27 +17,6 @@ router.post("/hdtv", verifyAdmin, addHDTVController);
 router.put("/hdtv/:id", verifyAdmin, updateHDTVController);
 router.delete("/hdtv/:id", verifyAdmin, deleteHDTVController);
 router.post("/hdtv/click/:id", handleHDTVClickController);
+router.get("/hdtv/custom/:title", getHDTVByTitleController); 
 
 export default router;
-
-// // routes\hdtv.js
-// import express from "express";
-// import {
-//     addHDTVController,
-//     updateHDTVController,
-//     deleteHDTVController,
-//     getAllHDTVController,
-//     handleHDTVClickController
-// } from "../controllers/hdtvController.js";
-
-// import { verifyAdmin } from "../middleware/auth.js";
-
-// const router = express.Router();
-
-// router.get("/hdtv", getAllHDTVController);
-// router.post("/hdtv/add", verifyAdmin, addHDTVController);
-// router.put("/hdtv/update", verifyAdmin, updateHDTVController);
-// router.delete("/hdtv/delete", verifyAdmin, deleteHDTVController);
-// router.post("/hdtv/click/:tmdbID", handleHDTVClickController);
-
-// export default router;
