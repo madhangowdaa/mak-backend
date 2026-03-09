b5dea546f1f2a49327ac50decdb50cf23f626b27
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-03-09 13:25:41 +0530

    Update server.js 4k

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ import statsRotes from "./routes/stats.js";
 import searchRoutes from "./routes/search.js";
 import hdtvRoutes from "./routes/hdtv.js";
 import backupRoutes from './routes/backup.js';
+import fourKMovieRoutes from "./routes/4kMovies.js";
+
 import rateLimit from 'express-rate-limit';
 
 const downloadLimiter = rateLimit({
@@ -48,6 +50,7 @@ app.use('/api', upcomingRoutes);
 app.use('/api',statsRotes);
 app.use('/api', backupRoutes);
 app.use("/api/movies/download", downloadLimiter);
+app.use("/api", fourKMovieRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
