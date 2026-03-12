4b950d465c6f34101bb646bd80ec218842dd142b
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2026-03-12 12:17:23 +0530

    Update carouselRoutes.js added new cmds to move , pin and unpin the carousel

--- routes/carouselRoutes.js
+++ routes/carouselRoutes.js
@@ -1,6 +1,6 @@
 // routes/carouselRoutes.js
 import express from "express";
-import { addCarouselController, deleteCarouselController, getCarouselController } from "../controllers/carouselController.js";
+import { addCarouselController, deleteCarouselController, getCarouselController, moveCarouselController, pinCarouselController, unpinCarouselController } from "../controllers/carouselController.js";
 import { verifyAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,8 @@ const router = express.Router();
 router.get("/", getCarouselController);          // get all active slides
 router.post("/add", verifyAdmin, addCarouselController);   // add slide
 router.delete("/delete", verifyAdmin, deleteCarouselController); // delete slide
+router.post("/move", verifyAdmin, moveCarouselController);
+router.post("/pin", verifyAdmin, pinCarouselController);
+router.post("/unpin", verifyAdmin, unpinCarouselController);
 
 export default router;
